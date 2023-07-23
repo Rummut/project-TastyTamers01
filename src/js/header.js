1c52dc5c3b3c9587852432e52d7d2cb99ab62c50
Rummut <123414878+Rummut@users.noreply.github.com>
2023-07-23 21:36:08 +0300

    Revert "Header +"

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,3 @@
-import bodyScrollLock from 'body-scroll-lock';
-
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
