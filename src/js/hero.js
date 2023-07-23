ab340ca8d7f262f6b431e0130ffecb877706bf47
Olexandra2209 <123394674+Olexandra2209@users.noreply.github.com>
2023-07-23 20:15:41 +0300

    add csshero

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,52 +1,49 @@
-// import Swiper from 'swiper';
+import Swiper from 'swiper';
 
-// const fetchMasterClasses = async () => {
-//   try {
-//     const response = await fetch(
-//       'https://tasty-treats-backend.p.goit.global/api/events'
-//     );
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error('Error fetching master classes:', error);
-//     return [];
-//   }
-// };
-// // Виклик функцій для отримання даних
-// const getData = async () => {
-//   const masterClasses = await fetchMasterClasses();
+const fetchMasterClasses = async () => {
+  try {
+    const response = await fetch(
+      'https://tasty-treats-backend.p.goit.global/api/events'
+    );
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching master classes:', error);
+    return [];
+  }
+};
 
-//   // Тут ви можете використовувати отримані дані для відображення слайдера або зробити інші необхідні дії з ними
-//   console.log('Master Classes:', masterClasses);
-// };
-
-// // Виклик функції для отримання даних
-// getData();
-
-import { fetchEvent } from '../../js/API_request/fechEvent';
-import TemplateArticles from '../../templates/Hero-events.hbs';
-import Swiper, { Pagination, Navigation, Autoplay } from 'swiper';
-import 'swiper/swiper-bundle.min.css';
-
-const parentContainer = document.querySelector('.events');
-
-fetchEvent().then(data => {
-  parentContainer.insertAdjacentHTML('beforeend', TemplateArticles(data));
-
-  const swiper = new Swiper('.swiper', {
-    modules: [Pagination, Navigation, Autoplay],
-    allowSlideNext: true,
-    pagination: {
-      el: '.page',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 1500,
-    },
-    speed: 800,
-    loop: true,
+const createSlides = masterClassData => {
+  const swiperWrapper = document.getElementById('masterClassList');
+  masterClassData.forEach(masterClass => {
+    const slide = document.createElement('div');
+    slide.classList.add('swiper-slide');
+    // Тут ви можете додати контент для кожного слайда, наприклад, назву майстер-класу
+    slide.textContent = masterClass.title;
+    swiperWrapper.appendChild(slide);
   });
-});
+};
+
+const initializeSwiper = () => {
+  new Swiper('.swiper-container', {
+    // Налаштування Swiper.js
+    slidesPerView: 3, // Кількість видимих слайдів одночасно
+    spaceBetween: 20, // Відстань між слайдами
+    loop: true, // Перемотка слайдів у безкінечному режимі
+    pagination: {
+      el: '.swiper-pagination', // Елемент, в якому буде відображатись пагінація
+      clickable: true, // Дозволити клікати на пагінації для перемикання слайдів
+    },
+  });
+};
+
+const getData = async () => {
+  const masterClasses = await fetchMasterClasses();
+  createSlides(masterClasses);
+  initializeSwiper();
+};
+
+getData();
