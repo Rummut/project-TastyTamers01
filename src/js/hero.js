787e88e91144f4dc5f6dd014b7f51cb6490fdfc7
Olexandra2209 <123394674+Olexandra2209@users.noreply.github.com>
2023-07-23 22:33:32 +0300

    addcss

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Swiper from 'swiper';
 
 const fetchMasterClasses = async () => {
@@ -16,36 +17,53 @@ const fetchMasterClasses = async () => {
   }
 };
 
-const createSlides = masterClassData => {
-  const swiperWrapper = document.getElementById('masterClassList');
-  masterClassData.forEach(masterClass => {
-    const slide = document.createElement('div');
-    slide.classList.add('swiper-slide');
+const renderMasterClasses = masterClasses => {
+  const masterClassListElement = document.getElementById('masterClassList');
+  masterClassListElement.innerHTML = ''; // Очищаємо вміст перед додаванням нових слайдів
 
-    // Тут ви можете додати контент для кожного слайда, наприклад, назву майстер-класу
-    slide.textContent = masterClass.title;
+  masterClasses.forEach(masterClass => {
+    if (masterClass.imageUrl && masterClass.title && masterClass.description) {
+      const slideItem = document.createElement('div');
+      slideItem.classList.add('swiper-slide');
 
-    swiperWrapper.appendChild(slide);
+      // Тут ви можете створити HTML-структуру для кожного майстер-класу
+      // Наприклад:
+      slideItem.innerHTML = `
+        <div class="slider-item">
+          <img src="${masterClass.imageUrl}" alt="${masterClass.title}">
+          <h3>${masterClass.title}</h3>
+          <p>${masterClass.description}</p>
+        </div>
+      `;
+
+      masterClassListElement.appendChild(slideItem);
+    }
   });
 };
 
-const initializeSwiper = () => {
-  new Swiper('.swiper-container', {
-    // Налаштування Swiper.js
-    slidesPerView: 3, // Кількість видимих слайдів одночасно
-    spaceBetween: 20, // Відстань між слайдами
-    loop: true, // Перемотка слайдів у безкінечному режимі
-    pagination: {
-      el: '.swiper-pagination', // Елемент, в якому буде відображатись пагінація
-      clickable: true, // Дозволити клікати на пагінації для перемикання слайдів
-    },
-  });
+const initSlider = async () => {
+  try {
+    const masterClasses = await fetchMasterClasses();
+    renderMasterClasses(masterClasses);
+
+    new Swiper('.swiper-container', {
+      // Налаштування Swiper.js (за бажанням)
+      // Наприклад, якщо вам потрібні пагінація та навігація:
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } catch (error) {
+    console.error('Error initializing slider:', error);
+  }
 };
 
-const getData = async () => {
-  const masterClasses = await fetchMasterClasses();
-  createSlides(masterClasses);
-  initializeSwiper();
-};
-
-getData();
+// Виклик функції для ініціалізації слайдера
+initSlider();
+// document.addEventListener('DOMContentLoaded', () => {
+//   initSlider();
+// });
