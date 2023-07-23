aa466b1133d45873b221dc28091eee1fbe63408a
Rummut <kzantip@gmail.com>
2023-07-23 12:40:05 +0300

    add popular

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,4 +1,4 @@
-import TastyTreatsAPI from './tasty-treats-api';
+import TastyTreatsAPI from './API-request/popular-request';
 const getRequest = new TastyTreatsAPI();
 const refs = {
     list: document.querySelector('.popular-recipes-list'),
@@ -6,44 +6,34 @@ const refs = {
 }
 
 async function fetchPopular() {
-    let r = await fetch('https://tasty-treats-backend.p.goit.global/api/recipes/popular');
-    let arrayResp = await r.json();
-    return arrayResp;
-}
-
-function renderPopular(arrayResp) {
-    const markup = arrayResp.map(({ title, description, preview, _id }) => {
-        return `
-        
-         <li class="popular-recipes-item" data-id="${_id}">
+  const recipes = await getRequest.getRecipesData();
+  // async function fetchPopular() {
+  //     let r = await fetch('https://tasty-treats-backend.p.goit.global/api/recipes/popular');
+  //     let arrayResp = await r.json();
+  //     return arrayResp;
+  // }
+  const markup = recipes.map(el => {
+    return `<li class="popular-recipes-item" data-id="${el._id}">
       <div class="popular-recipes-wraper">
         <img
           class="popular-recipes-img"
-          src="${preview}"
-          alt="${title}"
+          src="${el.preview}"
+          alt="${el.title}"
           loading="slow"
           height="64"
           width="64"
         />
       </div>
       <div class="popular-containet-def">
-        <h3 class="popular-recipes-title">${title}</h3>
-        <p class="popular-recipes-description">${description}</p>
+        <h3 class="popular-recipes-title">${el.title}</h3>
+        <p class="popular-recipes-description">${el.description}</p>
       </div>
-    </li>`;
-
-    }).join('');
-
-    refs.list.insertAdjacentHTML('beforeend', markup);
-    refs.list_mobile.insertAdjacentHTML('beforeend', markup);
+    </li>`}).join('');
+  refs.list.insertAdjacentHTML('beforeend', markup);
+  // // async function popularFetchAndRender() {
+  // //     const data = await fetchPopular();
+  // //     renderPopular(data);
+  // // }
 }
 
-async function popularFetchAndRender() {
-    const data = await fetchPopular();
-    renderPopular(data);
-}
-
-popularFetchAndRender();
-
-
-// no add event listener !!!!!!!
+fetchPopular()
