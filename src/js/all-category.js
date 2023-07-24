3000d97b77bf79b5e885b65808ee320b01cef640
RuslanMelnykUA <indigo211212@gmail.com>
2023-07-24 20:53:38 +0300

    add

--- src/js/all-category.js
+++ src/js/all-category.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const categoriesList = document.querySelector('.categories-list');
+// const categoriesList = document.querySelector('.categories-list');
 
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/categories';
+// const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/categories';
 
-export async function getCategories() {
-  try {
-    const response = await axios.get(BASE_URL);
-    const markUp = createMarkUp(response.data);
-    if (categoriesList) {
-      categoriesList.innerHTML = markUp;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+// export async function getCategories() {
+//   try {
+//     const response = await axios.get(BASE_URL);
+//     const markUp = createMarkUp(response.data);
+//     if (categoriesList) {
+//       categoriesList.innerHTML = markUp;
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-getCategories();
+// getCategories();
 
-function createMarkUp(data) {
-  return data
-    .map(
-      ({ name }) => `<li class="category-item">
-              <p class="category-name">${name}</p>
-            </li>`
-    )
-    .join('');
-}
+// function createMarkUp(data) {
+//   return data
+//     .map(
+//       ({ name }) => `<li class="category-item">
+//               <p class="category-name">${name}</p>
+//             </li>`
+//     )
+//     .join('');
+// }
