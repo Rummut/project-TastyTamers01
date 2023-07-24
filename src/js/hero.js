db538513f3d67ad90b0cf5b0370e9674d130d772
Olexandra2209 <123394674+Olexandra2209@users.noreply.github.com>
2023-07-24 22:17:33 +0300

    addhtm;

--- src/js/hero.js
+++ src/js/hero.js
@@ -6,60 +6,94 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const fetchMasterClasses = async () => {
-  try {
-    const response = await fetch(
-      'https://tasty-treats-backend.p.goit.global/api/events'
+const swiper = document.querySelector('.swiper-container');
+console.log(swiper);
+
+fetchMasterClasses().then(answers => {
+  console.log(answers);
+  const serchElement = answers.map(answer => {
+    const image = answer.cook.imgUrl;
+    const imgDish = answer.topic.imgUrl;
+    const area = answer.topic.area;
+    const name = answer.topic.name;
+    swiper.insertAdjacentHTML(
+      'beforeend',
+      ` <div class="slider-item">
+//           <img class="slider-cook" src="${image}" alt="mastershef">
+ <img class="slider-dish" src="${imgDish}" alt="dish">
+//           <h3 class="slider-name">${name}</h3>
+//           <p class="slider-area">${area}</p>
+//         </div>`
     );
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching master classes:', error);
-    return [];
-  }
-};
-
-const renderMasterClasses = masterClasses => {
-  const masterClassListElement = document.getElementById('masterClassList');
-  masterClassListElement.innerHTML = '';
-  masterClasses.forEach(masterClass => {
-    if (masterClass.imageUrl && masterClass.title && masterClass.description) {
-      const slideItem = document.createElement('div');
-      slideItem.classList.add('swiper-slide');
-
-      slideItem.innerHTML = `
-        <div class="slider-item">
-          <img src="${masterClass.imageUrl}" alt="${masterClass.title}">
-          <h3>${masterClass.title}</h3>
-          <p>${masterClass.description}</p>
-        </div>
-      `;
-
-      masterClassListElement.appendChild(slideItem);
-    }
+    // console.log(image);
+    // console.log(imgDish);
+    // console.log(area);
+    // console.log(name);
   });
-};
+});
 
-const initSlider = async () => {
-  try {
-    const masterClasses = await fetchMasterClasses();
-    renderMasterClasses(masterClasses);
+async function fetchMasterClasses() {
+  const responce = await axios(
+    `https://tasty-treats-backend.p.goit.global/api/events`
+  );
+  console.log(responce.data);
+  return responce.data;
+}
 
-    const swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  } catch (error) {
-    console.error('Error initializing slider:', error);
-  }
-};
+// const fetchMasterClasses = async () => {
+//   try {
+//     const response = await fetch(
+//       'https://tasty-treats-backend.p.goit.global/api/events'
+//     );
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+// //     console.error('Error fetching master classes:', error);
+// //     return [];
+// //   }
+// // };
 
-initSlider();
+// const renderMasterClasses = masterClasses => {
+//   const masterClassListElement = document.getElementById('masterClassList');
+//   masterClassListElement.innerHTML = '';
+//   masterClasses.forEach(masterClass => {
+//     if (masterClass.imageUrl && masterClass.title && masterClass.description) {
+//       const slideItem = document.createElement('div');
+//       slideItem.classList.add('swiper-slide');
+
+//       slideItem.innerHTML = `
+//         <div class="slider-item">
+//           <img src="${masterClass.imageUrl}" alt="${masterClass.title}">
+//           <h3>${masterClass.title}</h3>
+//           <p>${masterClass.description}</p>
+//         </div>
+//       `;
+
+//       masterClassListElement.appendChild(slideItem);
+//     }
+//   });
+// };
+
+// const initSlider = async () => {
+//   try {
+//     const masterClasses = await fetchMasterClasses();
+//     renderMasterClasses(masterClasses);
+
+//     const swiper = new Swiper('.swiper-container', {
+//       pagination: {
+//         el: '.swiper-pagination',
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     });
+//   } catch (error) {
+//     console.error('Error initializing slider:', error);
+//   }
+// };
+
+// initSlider();
