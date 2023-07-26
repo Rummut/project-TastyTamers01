09d53a3898e910c71c913fbd85d3b9a58cf50c97
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2023-07-26 20:35:46 +0200

    update ...

--- src/js/all-category-favorite.js
+++ src/js/all-category-favorite.js
@@ -3,11 +3,13 @@
   const favoriteBtnList = document.querySelector('.hero-favorite-categories');
   const heroFavoriteContent = document.querySelector('.hero-favorite-content');
 
+  
   /* if (renderCardsList.children.length === 0) {
     heroFavoriteContent.style.display = 'none';
   } else {
     heroFavoriteContent.style.display = 'block';
   } */
+
 
 function renderCards(cardData) {
 
