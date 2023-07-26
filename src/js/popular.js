b297784de41666b79bb44a71e1448bc9edf637b4
Rummut <kzantip@gmail.com>
2023-07-27 02:17:26 +0300

    add link in popular section

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,16 +1,20 @@
 import TastyTreatsAPI from './API-request/popular-request';
+import { openModal } from './modal-recipes';
 const getRequest = new TastyTreatsAPI();
 const refs = {
-    list: document.querySelector('.popular-recipes-list'),
+  list: document.querySelector('.popular-recipes-list'),
   list_mobile: document.querySelector('.popular-list.popular-recipes-mobile'),
-    modal: document.querySelector("[data-modal]"),
-}
+  modal: document.querySelector('[data-modal]'),
+};
+
+refs.list.addEventListener('click', fetchModal);
 
 async function fetchPopular() {
   const recipes = await getRequest.getRecipesData();
- 
-  const markup = recipes.map(el => {
-    return `<li class="popular-recipes-item" data-id="${el._id}">
+  console.log(recipes);
+  const markup = recipes
+    .map(el => {
+      return `<li class="popular-recipes-item" data-id="${el._id}">
       <div class="popular-recipes-wraper">
         <img
           class="popular-recipes-img"
@@ -23,14 +27,17 @@ async function fetchPopular() {
         <h3 class="popular-recipes-title">${el.title}</h3>
         <p class="popular-recipes-description">${el.description}</p>
       </div>
-    </li>`}).join('');
-  refs.list.insertAdjacentHTML('beforeend', markup);
- 
+    </li>`;
+    })
+    .join('');
+  refs.list.insertAdjacentHTML('afterbegin', markup);
 }
+fetchPopular();
 
-fetchPopular()
-
-refs.list.addEventListener('click', ()=> {
-   refs.modal.classList.add("is-hidden");
-  document.body.style.overflowY = 'auto'
-})
+async function fetchModal() {
+  const recipes = await getRequest.getRecipesData();
+  console.log(recipes);
+  const calls = recipes.map(el => {
+    refs.list.addEventListener('click', openModal(`${el._id}`));
+  });
+}
