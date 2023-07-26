a411c58977c682e6825ab3eb073727adc632abd2
olgafrostpl <123272546+olgafrostpl@users.noreply.github.com>
2023-07-26 23:28:14 +0300

    1st commit

---
+++ src/js/modal-order.js
@@ -0,0 +1,23 @@
+// const refs = {
+//   openModalOrder: document.querySelector('[hero-order-btn]'),
+//   openModalOrder: document.querySelector('.header-shopping-link'),
+//   closeModalOrder: document.querySelector('modal-order-close'),
+//   backDropOrder: document.querySelector('.backdrop-order'),
+//   modalOrder: document.querySelector('.modal-order'),
+// };
+
+// refs.openModalOrder.addEventListener('click', openModal);
+// refs.closeModalOrder.addEventListener('click', closeModal);
+// refs.backDropOrder.addEventListener('click', closeModal);
+
+// function openModal() {
+//   refs.modal.classList.remove('is-hidden');
+
+//   document.body.style.overflowY = 'hidden';
+// }
+
+// function closeModal() {
+//   refs.modal.classList.add('is-hidden');
+//   document.body.style.overflowY = 'auto';
+// }
+// export { openModal, refs };
