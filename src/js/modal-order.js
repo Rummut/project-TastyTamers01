56f72eb62f7d53786211d4e06430f153957d7cef
olgafrostpl <123272546+olgafrostpl@users.noreply.github.com>
2023-07-27 11:06:26 +0300

    add modal-order
    Зверніть увагу, що через JS ціє модалки не відкривається модалка з рецептами!!! Хоча усі змінні перейменовані.
    
    Працюю далі над стилями форми всередині модалки

--- src/js/modal-order.js
+++ src/js/modal-order.js
@@ -1,13 +1,58 @@
-
 const refs = {
-  openModalBtn: document.querySelector("[data-order-open]"),
-  closeModalBtn: document.querySelector("[data-order-close]"),
-  modal: document.querySelector("[data-order]"),
+  openOrderBtn: document.querySelector('.header-shopping-link'),
+  openOrderHero: document.querySelector('.hero-order-btn'),
+  closeOrderBtn: document.querySelector('.order-modal-close-btn'),
+  backdropOrder: document.querySelector('.backdrop-order'),
+  modalOrder: document.querySelector('.modal-order'),
+  forma: document.querySelector('.modal-form-order'),
 };
+try {
+  refs.openOrderHero.addEventListener('click', openModalOpen);
+} catch (error) {}
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openOrderBtn.addEventListener('click', openModalOpen);
+refs.closeOrderBtn.addEventListener('click', closeModalClose);
+refs.backdropOrder.addEventListener('click', clickBackdropClick);
 
-function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
+refs.forma.addEventListener('submit', sendOrder);
+function sendOrder(e) {
+  e.preventDefault();
+  const { name, tel, email, comment } = e.currentTarget;
+  const result = {
+    name: name.value,
+    tel: tel.value,
+    email: email.value,
+    comment: comment.value,
+  };
+
+  console.log(result);
+  e.currentTarget.reset();
+  closeModalClose();
+}
+
+function openModalOpen() {
+  window.addEventListener('keydown', onEscPress);
+  document.body.classList.add('overflowHidden');
+  refs.backdropOrder.classList.add('active');
+  refs.modalOrder.classList.add('active');
+}
+
+function closeModalClose() {
+  document.body.classList.remove('overflowHidden');
+  window.removeEventListener('keydown', onEscPress);
+  document.body.classList.remove('overflowHidden');
+  refs.backdropOrder.classList.remove('active');
+  refs.modalOrder.classList.remove('active');
+}
+
+function clickBackdropClick(e) {
+  if (e.currentTarget === e.target) {
+    closeModalClose();
+  }
+}
+
+function onEscPress(e) {
+  if (e.code === 'Escape') {
+    closeModalClose();
+  }
 }
